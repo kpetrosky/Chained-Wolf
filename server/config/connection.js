66696562df3ac014e49b47d396cc44c39e8ae094
kpetrosky <kpetrosky36@gmail.com>
2023-07-04 15:53:48 -0700

    added seeds and continue trying to run seed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Chained Wolfe MetalCraft', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 module.exports = mongoose.connection;
+const mongoose = require('mongoose');
+
+const databaseName = 'Chained Wolfe MetalCrafts'; // Replace 'my_database' with your desired database name without any spaces
+
+mongoose.connect(`mongodb://localhost/${databaseName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to the database');
+    // Your further code here
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
