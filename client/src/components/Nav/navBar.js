051f48a59ae5f5f6ef6a7afd49bd7d38e66eaa9a
kpetrosky <kpetrosky36@gmail.com>
2023-07-05 06:14:34 -0700

    trying to get the front to show in local host

---
+++ client/src/components/Nav/navBar.js
@@ -0,0 +1,24 @@
+// Get the necessary DOM elements
+// const navToggle = document.querySelector('.nav-toggle');
+// const navMenu = document.querySelector('.nav-menu');
+// const navLinks = document.querySelectorAll('.nav-menu a');
+
+// // Add event listeners and define the functionality
+// navToggle.addEventListener('click', () => {
+//   navMenu.classList.toggle('nav-menu-active');
+// });
+
+// navLinks.forEach((link) => {
+//   link.addEventListener('click', () => {
+//     // Remove the active class from all menu items
+//     navLinks.forEach((item) => {
+//       item.classList.remove('active');
+//     });
+
+//     // Add the active class to the clicked menu item
+//     link.classList.add('active');
+
+//     // Close the mobile menu if it's open (optional)
+//     navMenu.classList.remove('nav-menu-active');
+//   });
+// });
